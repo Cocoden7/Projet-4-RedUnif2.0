9c1b474deb1034be08fc7bc2357ec2c72a7fb5fc
ahennecart <ahennecart@oasis.uclouvain.be>
2020-03-22 12:09:51 +0100

    Correction de bugs sur le piston

--- Assets/Ressources/Scripts/PistonBehavior.cs
+++ Assets/Ressources/Scripts/PistonBehavior.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PistonBehavior : MonoBehaviour
 {
     private Vector2 direction;
+    private Vector2 dir16;
     private Rigidbody2D rbManche;
     private Rigidbody2D rbBout;
-    private Vector2 dir16;
+    private BoxCollider2D colDet;
 
     public Rigidbody2D rb;
     public GameObject Manche;
     public GameObject Bout;
+    public GameObject Detecteur;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,7 @@ public class PistonBehavior : MonoBehaviour
         QuelleDirection();
         rbManche = Manche.GetComponent<Rigidbody2D>();
         rbBout = Bout.GetComponent<Rigidbody2D>();
+        colDet = Detecteur.GetComponent<BoxCollider2D>();
         StartCoroutine(Movement());
     }
 
@@ -32,6 +35,7 @@ public class PistonBehavior : MonoBehaviour
         {
             // On etend le piston
             yield return new WaitForSeconds(1.0f);
+            colDet.enabled = false;
             rbBout.MovePosition(rbBout.position += direction);
             rbManche.MovePosition(rbManche.position += (direction - dir16));
             yield return new WaitForSeconds(0.05f);
@@ -45,12 +49,31 @@ public class PistonBehavior : MonoBehaviour
             yield return new WaitForSeconds(0.05f);
             rbBout.MovePosition(rbBout.position -= direction);
             rbManche.MovePosition(rbManche.position -= (direction - dir16));
+            colDet.enabled = true;
         }
     }
 
     void QuelleDirection()
     {
-        direction.x = 0.5f;
-        dir16.x = 1/32f;
+        if(rb.tag == "PistonDroite")
+        {
+            direction.x = 0.5f;
+            dir16.x = 1 / 32f;
+        }
+        else if (rb.tag == "PistonGauche")
+        {
+            direction.x = -0.5f;
+            dir16.x = -1 / 32f;
+        }
+        else if (rb.tag == "PistonHaut")
+        {
+            direction.y = 0.5f;
+            dir16.y = 1 / 32f;
+        }
+        else if (rb.tag == "PistonBas")
+        {
+            direction.y = -0.5f;
+            dir16.y = -1 / 32f;
+        }
     }
 }
