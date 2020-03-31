2494f2469c90f77de8f9ac4be43932f9d2f4ab0b
maximevolders <maxime.volders@hotmail.be>
2020-03-31 18:00:35 +0200

    Tutoriel entier
    Faut encore modifier la ligne de 9 et mettre le tuto en première scène tout en vérifiant qu'il se lance qu'une seule fois

--- Assets/Ressources/Scripts/TutorialManager.cs
+++ Assets/Ressources/Scripts/TutorialManager.cs
@@ -10,13 +10,14 @@ public class TutorialManager : MonoBehaviour
     public GameObject[] popUps;
     public GameObject credit;
     public GameObject buttons;
-    public GameObject nextText;
+    public GameObject text;
     public Button next;
 
     private float waitTime = 4.0f;
     private int popUpIndex = 0;
     private Boolean moveDone = false;
     private Boolean coinDone = false;
+    private Boolean coin = true;
 
     public void Update()
     {
@@ -34,13 +35,13 @@ public class TutorialManager : MonoBehaviour
         if(popUpIndex == 0)
         {
             next.interactable = true;
-            //nextText.SetActive(true);
+            text.SetActive(true);
         }
         if(popUpIndex == 2)
         {
             buttons.SetActive(true);
             next.interactable = false;
-            //nextText.SetActive(false);
+            text.SetActive(false);
             if (moveDone)
             {
                 DoneMouvement(false);
@@ -48,24 +49,28 @@ public class TutorialManager : MonoBehaviour
                 popUpIndex++;
             }
         }
-        /*else if(popUpIndex == 3)
+        else if(popUpIndex == 3)
         {
-            credit.SetActive(true);
+            if (coin)
+            {
+                credit.SetActive(true);
+                coin = false;
+            }
             if (coinDone)
             {
                 DoneCredit(false);
                 popUpIndex++;
             }
-        }*/
+        }
         else if (popUpIndex == 4 || popUpIndex == 6 || popUpIndex == 8)
         {
             next.interactable = true;
-            //nextText.setActive(true);
+            text.SetActive(true);
         }
         else if(popUpIndex == 5 || popUpIndex == 7)
         {
             next.interactable = false;
-            //nextText.setActive(false);
+            text.SetActive(false);
             waitTime -= Time.deltaTime;
             if (waitTime < 0)
             {
@@ -83,7 +88,6 @@ public class TutorialManager : MonoBehaviour
     public void Next()
     {
         popUpIndex++;
-        print(popUpIndex);
     }
     void DoneMouvement(Boolean b)
     {
