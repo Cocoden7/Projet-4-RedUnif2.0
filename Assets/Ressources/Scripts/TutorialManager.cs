1d09af3a50e2fa81089fb6458bac77fb73b5cae4
maximevolders <maxime.volders@hotmail.be>
2020-03-31 15:42:34 +0200

    Tutoriel v2
    bugs à résoudre:
    1) Problème avec la collecte de crédits
    2) nextText.SetActive bug alors que c'est un gameObject UI
    3) manque une partie du préfab dead-line

--- Assets/Ressources/Scripts/TutorialManager.cs
+++ Assets/Ressources/Scripts/TutorialManager.cs
@@ -8,10 +8,15 @@ public class TutorialManager : MonoBehaviour
 {
 
     public GameObject[] popUps;
-    private int popUpIndex;
+    public GameObject credit;
     public GameObject buttons;
+    public GameObject nextText;
     public Button next;
-    public float waitTime = 2f;
+
+    private float waitTime = 4.0f;
+    private int popUpIndex = 0;
+    private Boolean moveDone = false;
+    private Boolean coinDone = false;
 
     public void Update()
     {
@@ -29,34 +34,47 @@ public class TutorialManager : MonoBehaviour
         if(popUpIndex == 0)
         {
             next.interactable = true;
+            //nextText.SetActive(true);
         }
-        /* if(popUpIndex == 2)
+        if(popUpIndex == 2)
         {
             buttons.SetActive(true);
             next.interactable = false;
-            if (pressAnyKey)
+            //nextText.SetActive(false);
+            if (moveDone)
             {
+                DoneMouvement(false);
+                next.interactable = true;
                 popUpIndex++;
             }
         }
-        else if(popUpIndex == 3)
+        /*else if(popUpIndex == 3)
         {
-            if (creditRecolte)
+            credit.SetActive(true);
+            if (coinDone)
             {
+                DoneCredit(false);
                 popUpIndex++;
             }
-        }
-        else if(popUpIndex == 4 || popUpIndex == 6 || popUpIndex == 8)
+        }*/
+        else if (popUpIndex == 4 || popUpIndex == 6 || popUpIndex == 8)
         {
             next.interactable = true;
+            //nextText.setActive(true);
         }
         else if(popUpIndex == 5 || popUpIndex == 7)
         {
             next.interactable = false;
+            //nextText.setActive(false);
             waitTime -= Time.deltaTime;
-            popUpIndex++;
-        } */
-        /* else */ if(popUpIndex == 14)
+            if (waitTime < 0)
+            {
+                popUpIndex++;
+                waitTime = 4.0f;
+            }
+
+        }
+        else if(popUpIndex == 14)
         {
             SceneManager.LoadScene(0);
         }
@@ -66,5 +84,13 @@ public class TutorialManager : MonoBehaviour
     {
         popUpIndex++;
         print(popUpIndex);
+    }
+    void DoneMouvement(Boolean b)
+    {
+        moveDone = b;
+    }
+    void DoneCredit(Boolean b)
+    {
+        coinDone = b;
     }
 }
