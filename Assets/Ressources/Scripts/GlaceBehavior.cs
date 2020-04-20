39e275687411dd25d6c6559c6199ae07e2dc150f
ahennecart <ahennecart@oasis.uclouvain.be>
2020-04-20 15:56:40 +0200

    Correction bug glace

--- Assets/Ressources/Scripts/GlaceBehavior.cs
+++ Assets/Ressources/Scripts/GlaceBehavior.cs
@@ -1,13 +1,42 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GlaceBehavior : MonoBehaviour
 {
+    private Collider2D thisCol;
+    private bool actif = true;
+
+    void Start()
+    {
+        thisCol = GetComponent<Collider2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.tag == "Player")
+        if (col.tag == "Player")
         {
-            col.GetComponent<PlayerBehavior>().Avance();
+            PlayerBehavior player = col.GetComponent<PlayerBehavior>();
+            if (!player.surglace && actif)
+            {
+                player.Avance();
+            }
+            if (actif)
+            {
+                player.surglace = true;
+                StartCoroutine(Attente(col));
+            }
         }
+    }
+
+    IEnumerator Attente(Collider2D colPlayer)
+    {
+        actif = false;
+        while (thisCol.IsTouching(colPlayer))
+        {
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return new WaitForSeconds(0.5f);
+        actif = true;
     }
 }
--- Assets/Ressources/Scripts/StagePlayerBehavior.cs
+++ Assets/Ressources/Scripts/StagePlayerBehavior.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 
 // Gère toutes les collisions du joueur, ses mouvements (clavier), sa mort, la collecte de crédits, lance le niveau suivant lorsque nécessaire
 
