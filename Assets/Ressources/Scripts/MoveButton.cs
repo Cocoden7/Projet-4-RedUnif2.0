7b8eb4303f62c76759f75c291c1fcf6ddccc7d92
maximevolders <maxime.volders@hotmail.be>
2020-04-28 16:02:00 +0200

    Tutoriel plus court
    J'ai expliqué qu'on perdait 1 point par mort et j'ai raccourcit le tutoriel (on est passé de 14 éléments à 10, c'est pas dingue mais je peux pas faire plus court)

--- Assets/Ressources/Scripts/MoveButton.cs
+++ Assets/Ressources/Scripts/MoveButton.cs
@@ -7,47 +7,29 @@ public class MoveButton : MonoBehaviour
 {
 	Vector2 vec;
 	public Rigidbody2D Player;
-    private GameObject Tuto;
 	
     void Start()
     {
         Player = FindObjectOfType<PlayerBehavior>().rb;
-        Tuto = FindObjectOfType<TutorialManager>().gameObject;
     }
 
     public void Up()
     {
         Player.SendMessageUpwards("MoveUp", SendMessageOptions.DontRequireReceiver);
-        if (SceneManager.GetActiveScene().name == "Tutorial")
-        {
-            Tuto.SendMessageUpwards("DoneMouvement", true, SendMessageOptions.DontRequireReceiver);
-        }
     }
 
     public void Down()
     {
         Player.SendMessageUpwards("MoveDown", SendMessageOptions.DontRequireReceiver);
-        if (SceneManager.GetActiveScene().name == "Tutorial")
-        {
-            Tuto.SendMessageUpwards("DoneMouvement", true, SendMessageOptions.DontRequireReceiver);
-        }
     }
 
     public void Left()
     {
         Player.SendMessageUpwards("MoveLeft", SendMessageOptions.DontRequireReceiver);
-        if (SceneManager.GetActiveScene().name == "Tutorial")
-        {
-            Tuto.SendMessageUpwards("DoneMouvement", true, SendMessageOptions.DontRequireReceiver);
-        }
     }
 
     public void Right()
     {
         Player.SendMessageUpwards("MoveRight", SendMessageOptions.DontRequireReceiver);
-        if (SceneManager.GetActiveScene().name == "Tutorial")
-        {
-            Tuto.SendMessageUpwards("DoneMouvement", true, SendMessageOptions.DontRequireReceiver);
-        }
     }
 }
